9dae0d41476296a62be63fd7082832e15421f9ad
GauravB159 <gbolosta@gmail.com>
2017-07-10 19:10:52 +0530

    Multiseries Line Chart

--- line/src/js/line.js
+++ line/src/js/line.js
@@ -16,7 +16,7 @@ var LineGraph = function(config){
 }
 var d,x,y,xAxis,yAxis,chart,height,width,line,parseTimeZ;
 
-LineGraph.prototype.createLineGraph = function(){	
+LineGraph.prototype.createLineGraph = function(xaxis,yaxis){	
 	var angle = this.angle,
 		hoverLabel = this.hoverLabel,
 		ratio = this.ratio,
@@ -34,7 +34,7 @@ LineGraph.prototype.createLineGraph = function(){
 		rat;
 	parseDate = d3.timeParse("%Y-%m-%d");
 	formatTime = d3.timeFormat("%d %B %Y");
-	var bisectDate = d3.bisector(function(d) { return d.Date; }).left;
+	var bisectDate = d3.bisector(function(d) { return d[xaxis]; }).left;
 
 	var margin = {top: marginTop, right: marginRight, bottom: marginBottom, left: marginLeft};
 	if(height === undefined && width === undefined ){
@@ -59,7 +59,7 @@ LineGraph.prototype.createLineGraph = function(){
 	y = d3.scaleLinear()
 				.range([height,0]);
 	xAxis = d3.axisBottom(x).ticks(10);
-	yAxis = d3.axisLeft(y).ticks(parseInt(height / pad));
+	yAxis = d3.axisLeft(y).ticks(6).tickFormat(d3.format(".0f"));
 
 	var coordinates = [0, 0];
 	chart = d3.select('.chart');
@@ -73,92 +73,117 @@ LineGraph.prototype.createLineGraph = function(){
 					 .append('g')
 					 .attr('transform','translate(' + margin.left + ',' + margin.top + ')');
 	}
+	var color =  d3.scaleOrdinal(d3.schemeCategory10);
 
-	d3.csv('./data/data.csv',function(error,data){
+	var focus = chart.append("g")
+		      .attr("class", "focus")
+		      .style("display", "none");
+	if(!show_datap){
+		focus.append("circle")
+	  		.attr("r", 4.5);
+		}
+
+	focus.append("text")
+	    .attr("x", 9)
+	    .attr("dy", ".35em");
+
+	d3.csv('./data/data3.csv',function(error,data){
 		d = data;
-		data.forEach(function(d) {
-		    d.Date = parseDate(d.Date);
-		    d.Open = +d.Open;
-		  });
-
-		data.sort(function(a, b) {
-		    return a.Date - b.Date;
-		  });
-		x.domain(d3.extent(data,function(d){ return d.Date}));
-		y.domain(d3.extent(data,function(d){ return +d.Open}));
-		line = d3.line()
-			.x(function(d){ return x(d.Date)})
-			.y(function(d){ return y(d.Open)});
-		
+		var keys = d3.keys(data[0]).filter(function(key){ return key !== xaxis});
+		data.forEach(function(d){
+			d[xaxis] = parseDate(d[xaxis]);
+		});
+		var max = d[0][keys[0]];
+		var min = d[0][keys[0]];
+		data.forEach(function(d){
+			keys.forEach(function(key){
+				if(d[key] > max){
+					max = d[key];
+				}
+				if(d[key] < min){
+					min = d[key];
+				}
+			})
+		})
+		x.domain(d3.extent(data,function(d){ return d[xaxis]}));
+		y.domain([min-1,max+1]);
 		chart.append('g')
 			.attr('class','x axis')
 			.attr('transform','translate(0,' + height + ')')
 			.call(xAxis);
 
-    	var ticks = d3.selectAll('.x .tick').nodes().length;
-    	var padding = width/ticks;
-    	var max = 0;
-    	d3.selectAll('.x .tick').each(function(d){
-    			var check = d3.select(this).select('text').node().getComputedTextLength();
-    			if(check > max){
-    				max = check;
-    			}
-    		}
-    	);
-    	padding = parseInt(width/(ticks));
-		if( padding < max ){
-			xAxis.ticks( 7 );
-			d3.select('.x').call(xAxis);
-		}
-
 		chart.append('g')
 			.attr('class','y axis')
 			.call(yAxis);
-
-    	d3.select('.y.axis .tick text').attr("font-size","10px");
-
-			 
-		 var focus = chart.append("g")
-				      .attr("class", "focus")
-				      .style("display", "none");
-		if(!show_datap){
-			focus.append("circle")
-	      		.attr("r", 4.5);
-      	}
-
-	    focus.append("text")
-	        .attr("x", 9)
-	        .attr("dy", ".35em");
-
-	    if(show_datap){
-			chart.append('g').selectAll('circle')
-				 .data(data)
-				 .enter()
-				 .append('circle')
-				 .attr('r',2)
-				 .attr('cx',function(d){ return x(d.Date)})
-				 .attr('cy',function(d){return y(d.Open)})
-				 .attr('fill','red')
-				 .attr('stroke','red');
-		}
-
-		d3.selectAll('.x text').attr("id",function(d,i){
-			return "x"+i;
+		
+		data.sort(function(a, b) {
+			return a[xaxis] - b[xaxis];
 		});
-		chart.append("path")
-		      .datum(data)
-		      .attr("class", "line1")
-		      .attr("fill", "none")
-		      .attr("stroke", color)
-		      .attr("stroke-linejoin", "round")
-		      .attr("stroke-linecap", "round")
-		      .attr("stroke-width", stroke_width)
-		      .attr("d", line)
+		color.domain(keys.map(function(c,index) { return index; }));
 
-		chart.on("mouseout",function(){
-			d3.selectAll('.x .curdate').remove();
-			chart.selectAll(".curline").remove();
-		});
+		keys.forEach(function(key,index){
+
+			data.forEach(function(d) {
+			    d[key] = +d[key];
+			  });
+			line = d3.line()
+				.x(function(d){ return x(d[xaxis])})
+				.y(function(d){ return y(d[key])});
+			
+			
+	    	var ticks = d3.selectAll('.x .tick').nodes().length;
+	    	var padding = width/ticks;
+	    	var max = 0;
+	    	d3.selectAll('.x .tick').each(function(d){
+	    			var check = d3.select(this).select('text').node().getComputedTextLength();
+	    			if(check > max){
+	    				max = check;
+	    			}
+	    		}
+	    	);
+	    	padding = parseInt(width/(ticks));
+			if( padding < max ){
+				xAxis.ticks( 4 );
+				d3.select('.x').call(xAxis);
+			}
+
+			
+
+	    	d3.select('.y.axis .tick text').attr("font-size","10px");
+
+
+			d3.selectAll('.x text').attr("id",function(d,i){
+				return "x"+i;
+			});
+			chart.append("path")
+			      .datum(data)
+			      .attr("class", "line" + index)
+			      .attr("fill", "none")
+			      .style("stroke", function(d) { return color(index); })
+			      .attr("stroke-linejoin", "round")
+			      .attr("stroke-linecap", "round")
+			      .attr("stroke-width", stroke_width)
+			      .attr("d", line)
+
+		    if(show_datap){
+				chart.append('g').selectAll('circle')
+					 .data(data)
+					 .enter()
+					 .append('circle')
+					 .attr('r',2)
+					 .attr('cx',function(d){ return x(d[xaxis])})
+					 .attr('cy',function(d){return y(d[key])})
+					 .attr('fill','red')
+					 .attr('class',index)
+					 .attr('stroke', function(d){return d3.select('.line' + d3.select(this).attr('class')).style('stroke') })
+					 .attr('fill', function(d){return d3.select('.line' + d3.select(this).attr('class')).style('stroke') });
+			}
+
+			chart.on("mouseout",function(){
+				d3.selectAll('.x .curdate').remove();
+				chart.selectAll(".curline").remove();
+			});
+
 			chart.append("rect")
 			      .attr("class", "overlay")
 			      .attr("width", width)
@@ -167,52 +192,58 @@ LineGraph.prototype.createLineGraph = function(){
 			      .on("mouseout", function() { focus.style("display", "none"); })
 			      .on("mousemove", mousemove);
 
-			function mousemove() {
-				coordinates = d3.mouse(this);
-				var xc = coordinates[0];
-				var yc= height - coordinates[1];
-				var coord = {"x":xc,"y":yc}
-				var date = new Date(x.invert(xc));
-				date = formatTime(date);
-				d3.selectAll('.x .curdate').remove();
-				d3.select('.x')
-					.data(date)
-					.append('g')
-					.attr('class','curdate')
-					.attr('transform','translate(' + xc + ',0)')
-					.append('text')
-					.attr('y',29)
-					.text(date);
-				var pos = xc;
-			    var x0 = x.invert(d3.mouse(this)[0]),
-			        i = bisectDate(data, x0, 1),
-			        d0 = data[i - 1],
-			        d1 = data[i],
-			        d = x0 - d0.Date > d1.Date - x0 ? d1 : d0;
-			        formatTime = d3.timeFormat("%Y-%d-%m");
-			    	x0 = formatTime(x0);
-			    focus.attr("transform", "translate(" + x(d.Date) + "," + y(d.Open) + ")");
-			    focus.select("text").text(d.Open);
-			    
-			    chart.select(".curline").remove();
-				chart.append('line')
-						.attr("class","curline")
-					    .style('stroke', 'black')
-					    .style('stroke-width',1.5)
-					    .attr('x1', x(d.Date))
-					    .attr('y1', height)
-					    .attr('x2', x(d.Date))
-					    .attr('y2', y(d.Open));
-			  }
-		if(hoverLabel === true){
-			chart.selectAll('.x.axis .tick text').style("fill","none");
-			chart.selectAll('.x.axis .tick line').style("stroke","none");					
-			chart.selectAll('.line')
-			 .on("mouseover",function(d,i){
-			 	d3.select('#x'+i).style("fill","black");
-			 }).on("mouseout",function(d,i){
-			 	d3.select('#x'+i).style("fill","none");
-			 })
-		}
-	});		
+				function mousemove() {
+					coordinates = d3.mouse(this);
+					var xc = coordinates[0];
+					var yc= height - coordinates[1];
+					var coord = {"x":xc,"y":yc}
+					var date = new Date(x.invert(xc));
+					date = formatTime(date);
+					d3.selectAll('.x .curdate').remove();
+					d3.select('.x')
+						.data(date)
+						.append('g')
+						.attr('class','curdate')
+						.attr('transform','translate(' + xc + ',0)')
+						.append('text')
+						.attr('y',29)
+						.text(date);
+
+					var pos = xc;
+				    var x0 = x.invert(d3.mouse(this)[0]),
+				        i = bisectDate(data, x0, 1),
+				        d0 = data[i - 1],
+				        d1 = data[i],
+				        d = x0 - d0[xaxis] > d1[xaxis] - x0 ? d1 : d0;
+				        formatTime = d3.timeFormat("%Y-%d-%m");
+				    	x0 = formatTime(x0);
+				    	var max = 0;
+				    	keys.forEach(function(key){
+				    		if(d[key] > max )
+				    			max = d[key];
+				    	})
+				    focus.attr("transform", "translate(" + x(d[xaxis]) + "," + y(max) + ")");
+				    focus.select("text").text(d[key]);
+				    chart.select(".curline").remove();
+					chart.append('line')
+							.attr("class","curline")
+						    .style('stroke', 'black')
+						    .style('stroke-width',1.5)
+						    .attr('x1', x(d[xaxis]))
+						    .attr('y1', height)
+						    .attr('x2', x(d[xaxis]))
+						    .attr('y2', y(max));
+				  }
+			if(hoverLabel === true){
+				chart.selectAll('.x.axis .tick text').style("fill","none");
+				chart.selectAll('.x.axis .tick line').style("stroke","none");					
+				chart.selectAll('.line')
+				 .on("mouseover",function(d,i){
+				 	d3.select('#x'+i).style("fill","black");
+				 }).on("mouseout",function(d,i){
+				 	d3.select('#x'+i).style("fill","none");
+				 })
+			}
+		});
+	});	
 }
